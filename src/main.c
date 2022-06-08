fb965093cad1fe398a704db49d42c27c42ef442a
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2022-06-08 23:21:48 +0200

    history without empty lines

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ngda-sil <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 19:17:56 by ngda-sil          #+#    #+#             */
-/*   Updated: 2022/06/08 23:12:06 by ngda-sil         ###   ########.fr       */
+/*   Updated: 2022/06/08 23:20:29 by ngda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,14 @@ int	main(int ac, char **av, char **envp)
 	{
 		line = calloc(50, sizeof(char));
 		line = readline(user);
-		if (line)
+		if (ft_strncmp(line, "", 1))
 			add_history(line);
 		if (!ft_strncmp(line, "exit", 4))
 			break ;
+		free (line);
 	}
 	rl_clear_history();
-	free (line);
 	free (user);
+	free (line);
 	return (0);
 }

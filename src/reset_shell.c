ee7fd8655ca9c9e40478f49f07df44f87f1a7cd6
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2022-06-09 21:40:50 +0200

    merge

--- src/reset_shell.c
+++ src/reset_shell.c
@@ -4,7 +4,8 @@
 void	quit_shell(t_data *a) //vrmt temporaire pour tester
 {
 	free (a->prompt);
-	while (1);
+	//while (1);
+	rl_clear_history();
 	exit(1);
 }
 
@@ -12,6 +13,6 @@ void	reset_shell(t_data *a, char **env)
 {
 	(void)env;
 	ft_bzero(a, sizeof(t_data));
-	//a->env = env_into_list(env);
+	a->env = env_into_list(env);
 	a->prompt = get_prompt();
 }
